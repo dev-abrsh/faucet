a15266c4a3c95cbbc99711e6f9ce4845b4009716
devabrsh <dev.abrshh@gmail.com>
2022-05-29 16:17:09 +0300

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import axios from 'axios'
+import axios from "axios";
 import { useEffect } from "react";
 
 function App() {
-  
   useEffect(() => {
-   function fetchData() {
-     
- axios.post('https://faucetpay.io/api/v1/getbalance', {
-     "api_key": "c2aad08e717a2fc7a3bd50f3775ae609a6762819"
-   }).then(res=>console.log(res)).catch(err=>console.log(err))
-
-  }
-  fetchData()
-  },[]);
+    function fetchData() {
+      axios
+        .post("https://faucetpay.io/api/v1/balance", {
+          api_key: "c2aad08e717a2fc7a3bd50f3775ae609a6762819",
+          currency: "BTC",
+        })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+    fetchData();
+  }, []);
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
